c829ef1c3151493620c791df8ccac928731aca9c
kplaczek <kplaczek@wi.ps.pl>
2015-01-07 19:05:27 +0100

    poprawki i nowe funkcje

--- class.auchan.php
+++ class.auchan.php
@@ -13,20 +13,29 @@ class auchan {
     }
 
     public function getShops() {
-        $page = file_get_contents(self::BASE_URL . self::SHOPS_URL);
-        preg_match('#<div class="shops-columns">.*?</div>#ism', $page, $found);
-        preg_match_all('#href="(.*?)">(.*?)<\/a>#ism', $found[0], $found);
-        $this->shops = $found;
-        return $found[1];
+        if ($this->shops === array()) {
+            $page = file_get_contents(self::BASE_URL . self::SHOPS_URL);
+            preg_match('#<div class="shops-columns">.*?</div>#ism', $page, $found);
+            preg_match_all('#href="(.*?)">(.*?)<\/a>#ism', $found[0], $found);
+            $this->shops = $found;
+            
+            if ($found !== array()) {
+                $this->shops = $found;
+            }
+        }
+        return $this->shops;
+    }
+    
+    public function getCities(){
+        $shops = $this->getShops();
+        return $shops[2];
     }
 
-    
-    
     public function getPrices() {
         foreach ($this->shops[1] as $key => $shop) {
             $content = file_get_contents(self::BASE_URL . $shop);
-            preg_match_all('#<p class="boxType3-info">(E 95|E 98|ON|LPG): <span>(.*?)<\/span><\/p>#ism', $content, $found);
-            if (!empty($found[0])) {
+            $status = preg_match_all('#<p class="boxType3-info">(E 95|E 98|ON|LPG): <span>(\d,\d\d)#ism', $content, $found);
+            if ((bool)$status) {
                 $this->prices[$this->shops[2][$key]] = array_combine($found[1], $found[2]);
             }
         }
@@ -34,4 +43,5 @@ class auchan {
     }
 
 }
+
 ?>
